0542804f8480fc055751ef38475619ec0aa1ab64
Erik Lindström <erik-lindstrom94@hotmail.com>
2017-04-05 17:10:06 +0200

    cleaning up

--- js/answerCtrl.js
+++ js/answerCtrl.js
@@ -6,53 +6,50 @@ trumpOrDumpApp.controller('AnswerCtrl',function($scope, $routeParams, Trump, fir
 	
 	if($routeParams.ans == "trump"){
 			
-			if(Trump.getAnswer() == true){
-				firebase.setStatistics("right");
-				Trump.updateScore(Trump.getScore() + 1);
-				$scope.showAnswer = "Tremendous!";
-				$scope.imagePattern = "http://citizensfortrump.com/wp-content/uploads/2015/09/thumbs-up-trump.jpg";
-				var audio = new Audio('right.mp3');
-				audio.play();
-			}else{
-			firebase.setStatistics("wrong");
-			Trump.updateScore(Trump.getScore() - 5);
-			$scope.showAnswer = "Fake News!";
-			$scope.imagePattern = "https://i.imgflip.com/14c217.jpg?a414168";
-			var audio = new Audio('wrong.mp3');
+		if(Trump.getAnswer() == true){
+			firebase.setStatistics("right");
+			Trump.updateScore(Trump.getScore() + 1);
+			$scope.showAnswer = "Tremendous!";
+			$scope.imagePattern = "http://citizensfortrump.com/wp-content/uploads/2015/09/thumbs-up-trump.jpg";
+			var audio = new Audio('right.mp3');
 			audio.play();
-			};
-
-			firebase.myHighScore(function(data){
-				if(data < Trump.getScore()) {
-					firebase.setHighScore(Trump.getScore());
-				}
-			});
-			
 		}else{
-			
-			if(Trump.getAnswer() == false){
-				firebase.setStatistics("right");
-				Trump.updateScore(Trump.getScore() + 1);
-				$scope.showAnswer = "Tremendous!";
-				$scope.imagePattern = "http://citizensfortrump.com/wp-content/uploads/2015/09/thumbs-up-trump.jpg";
-				var audio = new Audio('right.mp3');
-				audio.play();
-			}else{
 			firebase.setStatistics("wrong");
 			Trump.updateScore(Trump.getScore() - 5);
 			$scope.showAnswer = "Fake News!";
 			$scope.imagePattern = "https://i.imgflip.com/14c217.jpg?a414168";
 			var audio = new Audio('wrong.mp3');
 			audio.play();
-			};
-
-			firebase.myHighScore(function(data){
-				if(data < Trump.getScore()) {
-					firebase.setHighScore(Trump.getScore());
-				}
-			});
-	
 		};
-	
-	
+
+		firebase.myHighScore(function(data){
+			if(data < Trump.getScore()) {
+				firebase.setHighScore(Trump.getScore());
+			}
+		});
+			
+	}else{
+
+		if(Trump.getAnswer() == false){
+			firebase.setStatistics("right");
+			Trump.updateScore(Trump.getScore() + 1);
+			$scope.showAnswer = "Tremendous!";
+			$scope.imagePattern = "http://citizensfortrump.com/wp-content/uploads/2015/09/thumbs-up-trump.jpg";
+			var audio = new Audio('right.mp3');
+			audio.play();
+		}else{
+			firebase.setStatistics("wrong");
+			Trump.updateScore(Trump.getScore() - 5);
+			$scope.showAnswer = "Fake News!";
+			$scope.imagePattern = "https://i.imgflip.com/14c217.jpg?a414168";
+			var audio = new Audio('wrong.mp3');
+			audio.play();
+		};
+
+		firebase.myHighScore(function(data){
+			if(data < Trump.getScore()) {
+				firebase.setHighScore(Trump.getScore());
+			}
+		});
+	};
 });
